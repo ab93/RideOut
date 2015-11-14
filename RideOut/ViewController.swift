3e098de14eff8b86eed4caa160d37d16f2bfd565
Gaurav Nijhara <gauravnijhara@guest-wireless-207-151-035-102.usc.edu>
2015-11-14 00:40:38 -0800

    Initial Commit

---
+++ RideOut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RideOut
+//
+//  Created by Gaurav Nijhara on 11/14/15.
+//  Copyright © 2015 Gaurav Nijhara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
